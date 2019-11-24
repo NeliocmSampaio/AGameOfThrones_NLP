ad6913fd37ba1fd7c35ed6659ac12d055169cf5c
Nélio Sampaio <neliocmsampaio@gmail.com>
2019-11-24 13:31:33 -0300

    organize the list files and create a readme

---
+++ data/elements/README.md
@@ -0,0 +1,6 @@
+These files contains lists of some elements of westeros like characters, rivers, forests and castles. The lists are used for preprocessing replacing the name in the books by some key names and for training the classifier.
+
+Files:
+<element>.txt :             contains the list with no preprocessing
+<element>_list.txt :        contains the preprocessed list
+<element>_dictionary.txt:   contains the name found in the book and the key to replace it
--- src/filter_castles_file.py
+++ src/filter_castles_file.py
@@ -4,21 +4,35 @@ from nltk.corpus import stopwords
 from nltk.corpus import RegexpTokenizer
 from nltk.corpus import brown
 
-import gensim
-
 from sklearn.decomposition import PCA
 
 from matplotlib import pyplot
 
-filepath = "../data/main_characters.txt"
-filetosave = "../data/main_characters_processed.txt"
+element = "main_characters"
 
-file = open(filepath, 'rb')
-savefile = open(filetosave, 'w')
+filepath = "../data/elements/" + element + ".txt"
+filetosave = "../data/elements/" + element + "_dictionary.txt"
 
-for line in file.readlines():
-    words = line.decode('ascii').lower().replace('\'', "").replace(" ", "_").replace("-", "_")
-    savefile.write(words)
+def clean_list():
+    file = open(filepath, 'rb')
+    savefile = open(filetosave, 'w')
 
-file.close()
-savefile.close()
+    for line in file.readlines():
+        words = line.decode('ascii').lower().replace('\'', "").replace(" ", "_").replace("-", "_")
+        savefile.write(words)
+
+    file.close()
+    savefile.close()
+
+def create_dictionary():
+    file = open(filepath, 'r')
+    savefile = open(filetosave, 'w')
+
+    for line in file.readlines():
+        words = line.lower().replace('\'', "").replace(" ", "_").replace("-", "_")
+        savefile.write(line.lower().replace('\'', "").replace('\n', '') + " : " + words)
+
+    file.close()
+    savefile.close()
+
+create_dictionary()
